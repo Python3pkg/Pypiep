2ab1e9b3f4e29ff72be10e1b5c406b20a31f182c
Yaolong Huang <airekans@gmail.com>
2013-10-20 23:15:08 +0800

    Add debugging output to Process.

--- pypipe.py
+++ pypipe.py
@@ -2,21 +2,30 @@ import subprocess
 import re
 import collections
 
+
 def sh(cmd, shell=True):
-    return Process(cmd, shell)
+    return Process(None, cmd, shell)
 
 class Stream(object):
     _stream_classes = {}
     
-    def __init__(self):
+    def __init__(self, stream):
         if not self._stream_classes:
-            self.__get_subclasses(Stream)
+            self.__reg_class(Stream)
+
+        cls = self.__class__
+        if cls.__name__ not in self._stream_classes:
+            self.__reg_class(cls)
         
-    def __get_subclasses(self, c):
-        for subclass in c.__subclasses__():
+        self._stream = stream
+        print stream.__class__.__name__, 'stream'
+        
+    def __reg_class(self, cls):
+        self._stream_classes[cls.__name__] = cls
+        for subclass in cls.__subclasses__():
             self._stream_classes[subclass.__name__] = subclass
-            self.__get_subclasses(subclass)
-    
+            self.__reg_class(subclass)
+
     def __getattr__(self, name):
         cap_name = name.capitalize()
         if cap_name in self._stream_classes:
@@ -24,14 +33,33 @@ class Stream(object):
             return lambda *args, **kwargs: cls(self, *args, **kwargs)
         else:
             return None
+    
+    def __iter__(self):
+        for elem in self._do_iter():
+            yield elem
+        
+        self._end_iter()
+    
+    def _do_iter(self):
+        raise NotImplemented
+    
+    def _end_iter(self):
+        self._do_end_iter()
+        print self.__class__.__name__, 'end iter'
+        if self._stream:
+            print 'call instream _end_iter' 
+            self._stream._end_iter()
+    
+    def _do_end_iter(self):
+        pass
 
 class Process(Stream):
     '''
     classdocs
     '''
 
-    def __init__(self, cmd, shell=True):
-        Stream.__init__(self)
+    def __init__(self, stream, cmd, shell=True):
+        Stream.__init__(self, stream)
         if shell:
             if not isinstance(cmd, str):
                 cmd = ' '.join(cmd)
@@ -42,57 +70,58 @@ class Process(Stream):
         self.__cmd = cmd
         self.__p = subprocess.Popen(cmd, shell=shell, stdout=subprocess.PIPE)
     
-    def __iter__(self):
+    def _do_iter(self):
         for line in self.__p.stdout.xreadlines():
             yield line
-        
-        self.__p.wait()
     
-        
+    def _do_end_iter(self):
+        self.__p.stdout.close()
+        self.__p.wait()
+        print self.__cmd, 'process done'
+
 class Grep(Stream):
     def __init__(self, stream, pattern):
-        Stream.__init__(self)
-        self.__stream = stream
+        Stream.__init__(self, stream)
         self.__regex = re.compile(pattern)
     
-    def __iter__(self):
-        for elem in self.__stream:
+    def _do_iter(self):
+        for elem in self._stream:
             if self.__regex.search(elem):
                 yield elem
 
 class Col(Stream):
     def __init__(self, stream, column, sep=' '):
-        Stream.__init__(self)
-        self.__stream = stream
+        Stream.__init__(self, stream)
         self.__column = column
         self.__sep = sep
     
-    def __iter__(self):
+    def _do_iter(self):
         col = self.__column
-        for line in self.__stream:
+        for line in self._stream:
             cols = line.split(self.__sep)
             yield cols[col] if col < len(cols) else ''
 
 class Head(Stream):
     def __init__(self, stream, line_num = 10):
-        Stream.__init__(self)
-        self.__stream = stream
+        Stream.__init__(self, stream)
         self.__line_num = line_num 
         
-    def __iter__(self):
-        for line_num, line in enumerate(self.__stream):
+    def _do_iter(self):
+        for line_num, line in enumerate(self._stream):
             if line_num < self.__line_num:
                 yield line
+            else:
+                return
+
             
 class Tail(Stream):
     def __init__(self, stream, line_num = 10):
-        Stream.__init__(self)
-        self.__stream = stream
+        Stream.__init__(self, stream)
         self.__line_num = line_num 
         
-    def __iter__(self):
+    def _do_iter(self):
         last_lines = collections.deque()
-        stream_iter = iter(self.__stream)
+        stream_iter = iter(self._stream)
         while len(last_lines) < self.__line_num:
             try:
                 last_lines.append(stream_iter.next())
@@ -105,5 +134,3 @@ class Tail(Stream):
 
         for line in last_lines:
             yield line
-            
-            
