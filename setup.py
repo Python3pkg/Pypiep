a53941582689ed3678f8b166d5b7f4e1c7d926b5
Yaolong Huang <airekans@gmail.com>
2014-09-27 20:32:50 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pypipe',
+      version='0.1',
+      description='Shell pipeline in Python',
+      author='Yaolong Huang',
+      author_email='airekans@gmail.com',
+      url='https://github.com/airekans/Pypipe',
+      py_modules=['pypipe']
+     )
